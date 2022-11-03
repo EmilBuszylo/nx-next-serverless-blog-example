20b76eefde9ebe7ac39b9d673b56fd34c38ddc26
Emil Buszyło <emil.buszylo@olx.pl>
2022-11-03 13:17:15 +0100

    reafactor: move SubscriptionBox to separate component

---
+++ apps/blog-frontend/components/SubscriptionBox.tsx
@@ -0,0 +1,12 @@
+export const SubscriptionBox = () => (
+  <div
+    className="mx-auto p-6 max-w-sm bg-white rounded-lg border border-border-light shadow-md my-12">
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-font-default">Unlimited Access</h5>
+    <p className="mb-3 font-normal text-font-light">Full article is allowed only for non free
+      users. If you want to read more please purchase subscription.</p>
+    <a href="#"
+       className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-accent-default rounded-lg hover:opacity-60 transition-[opacity]">
+      Purchase Now
+    </a>
+  </div>
+)
--- apps/blog-frontend/pages/blog/[slug].tsx
+++ apps/blog-frontend/pages/blog/[slug].tsx
@@ -8,6 +8,7 @@ import {PageHeader} from "../../components/Page/PageHeader";
 import {PageTitle} from "../../components/Page/PageTitle";
 import {PostImage} from "../../components/Post/PostImage";
 import {PostTag} from "../../components/Post/PostTag";
+import {SubscriptionBox} from "../../components/SubscriptionBox";
 
 const lorem = new LoremIpsum({
   sentencesPerParagraph: {
@@ -57,16 +58,7 @@ const BlogPostPage: React.FC<BlogPostPageProps> = ({data, errorCode}) => {
         <p className="font-semibold">
           {excerpt}
         </p>
-        <div
-          className="mx-auto p-6 max-w-sm bg-white rounded-lg border border-border-light shadow-md my-12">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-font-default">Unlimited Access</h5>
-          <p className="mb-3 font-normal text-font-light">Full article is allowed only for non free
-            users. If you want to read more please purchase subscription</p>
-          <a href="#"
-             className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-accent-default rounded-lg hover:opacity-60 transition-[opacity]">
-            Purchase Now
-          </a>
-        </div>
+        <SubscriptionBox/>
         <div className="flex flex-col space-y-6 blur-lg">
           {
             body.map(b => (
